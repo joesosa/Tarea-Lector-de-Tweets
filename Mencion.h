8a220976fd72407bb30a35264033c6a6cba0fe07
joesosa <44095239+joesosa@users.noreply.github.com>
2019-05-02 14:50:50 -0600

    Create Mencion.h

---
+++ Mencion.h
@@ -0,0 +1,14 @@
+#ifndef _menciona
+#define _menciona
+
+class Mencion{
+    private:
+        char* nombre;
+        int cantMencionadas;
+    public:  
+        Mencion(char*);
+        ~Mencion();
+};
+
+
+#endif
