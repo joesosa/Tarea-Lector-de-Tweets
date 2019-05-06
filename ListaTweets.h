e02f393788bd534c7aa24e85118f599a8d6ea0a0
joesosa <44095239+joesosa@users.noreply.github.com>
2019-05-06 15:41:01 -0600

    Create ListaTweets.h

---
+++ ListaTweets.h
@@ -0,0 +1,36 @@
+#ifndef _LISTATWEETS
+#define _LISTATWEETS
+#include<iostream>
+using namespace std;
+class Lista {
+   class Celda;    // Prototipo
+   private:
+      Celda * primera;
+	    Celda * ultima;
+      class Celda{
+	     public:
+        char * usuario;
+  		  int cantidadMenciones; // Nos va a ayudar a llevar un conteo de la cantidad de veces que un usuario ha sido mencionado por otro
+  			Celda * anterior;
+  			Celda * siguiente;
+  			Celda * abajo;
+  			Celda( char* , int );
+        void liberadorColumnas();
+  			~Celda();
+  			ostream & imprimir( ostream &);
+	     };
+	public:
+		Lista();
+		~Lista();
+		Lista & pushFront(char *, int);
+		Lista & pushBack(char *, int);
+    int vacia();
+    int compararNombres(char *, char *);
+    int existeEscritor(char *, char *);
+    int existeMencion(char *, char *);
+    Lista & insertar(char *, int);
+    Lista & insertarMencion(char *, char *);
+    void ordenarLista();
+		ostream & imprimir( ostream & );
+};
+#endif
