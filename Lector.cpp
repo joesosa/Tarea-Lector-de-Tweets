4beb0ce53eb1517f42394266d113cd7c0092dd64
joesosa <44095239+joesosa@users.noreply.github.com>
2019-05-06 15:38:41 -0600

    Create Lector.cpp

---
+++ Lector.cpp
@@ -0,0 +1,82 @@
+#include "Lector.h"
+#include <iostream>
+#include <ctype.h>
+#include <fstream>
+using namespace std;
+#define MAX 30
+Lector::Lector(char* nombre){
+	ifstream entrada(nombre);
+}
+
+
+char* Lector::lectorArrobas(ifstream& entrada){
+	char caracter = ' ';
+	entrada.get(caracter);
+	char * palabra = 0;
+	palabra = new char[MAX];
+	int cont = 0;
+    while( ((int)caracter >= 0 && (int)caracter < 10) || isalpha(caracter) || caracter == '_'){
+		palabra[cont++] = caracter;
+		entrada.get(caracter);
+	}
+	return palabra;
+}
+
+void Lector::lectorDeComas(ifstream& entrada){
+	char coma = ',', caracter = ' ';
+	int cont = 0, indice = 0;
+	char * escritorTweet;
+	char **menciones = new char *[MAX];
+	while(cont<14){
+		entrada.get(caracter);
+		if(caracter == ','){
+			cont++;
+			entrada.get(caracter);
+		}
+		if(cont==1){
+            for(int i = 0; i < MAX;++i){
+			    menciones[i] = new char[MAX];
+		    }
+			while(cont == 1){
+				entrada.get(caracter);
+				if(caracter == '@'){
+					char * mencionRecibida;//revisar
+					mencionRecibida = lectorArrobas(entrada); //revisar
+					if(mencionRecibida){
+						menciones[indice++] = mencionRecibida; 
+					}
+				}
+				if(caracter == ','){
+					cont++;
+				}
+			}
+		}
+		if(cont == 3){
+			escritorTweet = lectorArrobas(entrada);//revisar
+		}
+		//if que revisa si la lista del escritor del tweet, crea o no crea
+		//despuese se mete a esa lista y suma a los del vector o los crea
+		//mismo metodo con los del vector
+		//y se le suma al atributo de escritor
+		/*if(!lista.primera){
+			lista.pushFront(escritorTweet);
+		}
+		else{
+			if(!lista.existe(escritorTweet)){
+				lista.pushBack(escritorTweet);
+			}
+		}*/
+		for(int j = 50; j>= 0; --j){
+			delete menciones[j];
+		}
+		delete[] menciones;
+	}
+}
+
+void Lector::lectorTexto(){
+	while(entrada.eof()){
+		lectorDeComas(entrada);
+	}
+}
+	
+		
