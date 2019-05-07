c81dd5c7bb2655e8a8c7e971774f6e641a07a9bc
joesosa <44095239+joesosa@users.noreply.github.com>
2019-05-06 18:36:50 -0600

    Update Lector.h

--- Lector.h
+++ Lector.h
@@ -1,17 +1,21 @@
 #ifndef _Lector
 #define _Lector
 #include <fstream>
+#include "ListaTweets.h"
 using namespace std;
 class Lector{
     public:
-        void lectorDeComas();
-		char* lectorArrobas();
-        Lector(char*);
-        void lectorTexto();
+        void lectorDeComas(ifstream&);
+		char* lectorArrobas(ifstream&);
+        Lector(ifstream&);
+        void lectorTexto(ifstream&);
+        void leer();
 		
 		
 	private:	
 		ifstream entrada;
+        char letra;
+        Lista lista;
         
 };
 #endif
