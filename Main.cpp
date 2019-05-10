c6f5c8e84811909e8e4eb242268bc4cff8bf4dfc
joesosa <44095239+joesosa@users.noreply.github.com>
2019-05-09 18:57:20 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,23 @@
-#include "ListaTweets.h"
+
 #include "Lector.h"
 #include<iostream>
 #include <fstream>
 using namespace std;
 int main(int argc, char * * argv){
   int opcion = 1;
-  Lista lista;
+
   ifstream entrada(argv[1]);
-  Lector lector(entrada);
+  Lector lector;
+  while(!entrada.eof()){
+    lector.lectorDeComas(entrada);
+  }
   while(opcion != 2){
     if(opcion == 1){
       int cantMenciones = 0;
       char* nombre  = new char[30];
       cout<<"Digite el usuario por ser consultado: "<<endl; cin>>nombre;
       cout<<"Digite la cantidad de menciones que desea ver de tal usuario... "<<endl; cin>>cantMenciones;
-      lista.imprimir(cout,nombre,cantMenciones);
+      //lista.imprimir(cout,nombre,cantMenciones);
       cout<<"******************************************"<<endl;
       delete nombre;
       opcion = 3;
@@ -22,6 +25,6 @@ int main(int argc, char * * argv){
     cout<<"Que desea hacer?"<<endl;
     cout<<"1. Consultar menciones \n2. Salir"<<endl; cin>>opcion;
   }
- 
+ // exit(1);
 return 0;
 }
